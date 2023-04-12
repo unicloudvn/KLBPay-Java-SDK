bec785608850e07e22e3ebed0b13cc333d6af1f2
hoangkhanhson2000 <hoangkhanhson2000@gmail.com>
2023-04-12 18:13:48 +0700

    add demo example

---
+++ your-module/src/main/java/testpayment/TestPaymentExampleApplication.java
@@ -0,0 +1,13 @@
+package testpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPaymentExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPaymentExampleApplication.class, args);
+    }
+
+}
