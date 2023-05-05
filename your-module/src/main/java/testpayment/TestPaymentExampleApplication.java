e851fae20e65bbbf2bd57a440bfb15f42e5bc20d
hoangkhanhson2000 <66456579+hoangkhanhson2000@users.noreply.github.com>
2023-05-05 14:50:19 +0700

    Update TestPaymentExampleApplication.java

--- your-module/src/main/java/testpayment/TestPaymentExampleApplication.java
+++ your-module/src/main/java/testpayment/TestPaymentExampleApplication.java
@@ -38,11 +38,6 @@ public class TestPaymentExampleApplication {
         config.setSecretKey("KP50qwbWF+rktXv6Gr+QuRHIBmrqWXke+Yuu2tFD/KVc9Xkj9FHeyx8sIWeG+WTU1I/kMrZJz9NbUCgxrYbDLoUD+tTtyybdfR9Vzo3CGrG+lLoiUixiRdez2zD+UL0lpi48HGz+WJ42ilIO+B8ZkUdKYY71bUqik1Sa/ygm9obLKaye/GsQq4/88HIOAkjWwMJH24N+5Kp1KELZx99FHd84FdvILX2sphfT5IEw3vOJxAglqDHXIjN0neRrVdoBKbnBUs8FqzLuFOE0e216eQqHs7pFQI15yauSqI+DAgS2Fx0ZA2Lchn5193NYpuHmpIy8qTGJebm4v9rF9/NsEg==");
         config.setEncryptKey("CA51E201A56FFA44DC3D7C22A00724BD51FFEFD53F57963B1D925E65012BAE94");
 
-//        config.setHost("https://api-staging.kienlongbank.co/pay");
-//        config.setClientId("9a739ba0-7804-4281-818b-d7fe8a1253e1");
-//        config.setSecretKey("KTg9nG/fhKQcr0B686nzgbK4TIvoxpdvVtzKv+htOh35wN8a4eD+E4zT9FuoWOePqCpV9a46neehCNZigrqMEHkGul+AvWUBDVghVzmzJwlFaablTHDPhTQGz1/TAEDbbKEyuYaOZEeIjtSD3+z/h6ObI28RImF9bDYaWVNetO0RV9UtlWTfyY5S9qxdOJ0p10zmdi0Lrqpw4b7/sV5J0lyxgGVs5qG8YimSuPgQnkK+2jZrZ9w4neyNb0l0Mn25MZMyng8gj2vS7m205gaUXLmGxC45mrUbgjDKRg0a2BsXTADUGjDeZm2G09+skSO+oF7MgjmcIXYZXsAUu8qufw==");
-//        config.setEncryptKey("D877B4959ED17A3752482C37F8183BAFA1D796F8D92E593F26369AD7F578039F");
-
         config.setAcceptTimeDiff(2000);
 
         RestTemplate restTemplate = new RestTemplate();
